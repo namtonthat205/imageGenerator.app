e90dc855fa6119288cc49bdd57f5147cc21faeb4
namtonthat205 <namtonthat205@gmail.com>
2024-10-05 21:20:31 +0100

    another commit

--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -27,7 +27,7 @@ router.route('/').get(async (req, res) => {
 router.route('/').post(async (req, res) => {
   try {
     const { name, prompt, photo } = req.body;
-    const photoUrl = await cloudinary.uploader.upload(photo);
+    const photoUrl = await cloudinary.uploader.upload(photo, { secure: true });
 
     const newPost = await Post.create({
       name,
